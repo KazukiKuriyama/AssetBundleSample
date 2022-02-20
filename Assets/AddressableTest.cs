c567414bd87464330e5d273b00fbf03d548d7d8c
kazuki <kazuki0819kazuki@gmail.com>
2022-02-21 00:19:07 +0900

    first commit

---
+++ Assets/AddressableTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AddressableAssets;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using Cysharp.Threading.Tasks;
+
+public class AddressableTest : MonoBehaviour
+{
+}
